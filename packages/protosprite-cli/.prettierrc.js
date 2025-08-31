4b0d4f64d62189b1d7c4b671505612c883ccaf2a
Robert Brownstein <robert.brownstein@gmail.com>
2025-08-31 07:37:47 -0400

    Updates

---
+++ packages/protosprite-cli/.prettierrc.js
@@ -0,0 +1,11 @@
+export default {
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrderParserPlugins: ["typescript"],
+  semi: true,
+  trailingComma: "none",
+  singleQuote: false,
+  printWidth: 80,
+  importOrder: ["<THIRD_PARTY_MODULES>", "^src/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+};
